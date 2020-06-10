3a6c103d4efd38efa03375254e2a9c46458340ae
Vlad <vlad300820001@gmail.com>
2020-06-10 10:18:41 +0300

    initial commit

---
+++ src/main/java/com/example/exam/ExamApplication.java
@@ -0,0 +1,17 @@
+package com.example.exam;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamApplication implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+    }
+}
